de4a3347d1db32d81d066ad0f6bc19254973a16f
MarikaKononen <marika.kononen@koodielamastani.fi>
2019-09-17 12:51:17 +0300

    uibuild

---
+++ build/precache-manifest.7d18f729f8e0907c364b96eb5c695620.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b23470658258934a8fc3a6f5b8f3e6e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56107d238084c300f867",
+    "url": "/static/css/main.1a3c9a2f.chunk.css"
+  },
+  {
+    "revision": "6a19a0beea039a280f26",
+    "url": "/static/js/2.3c62b441.chunk.js"
+  },
+  {
+    "revision": "56107d238084c300f867",
+    "url": "/static/js/main.a773473c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cors       = require('cors')
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :person'))
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
       {
